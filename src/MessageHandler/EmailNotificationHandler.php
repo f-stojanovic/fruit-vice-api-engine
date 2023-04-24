2301fe9324e2291736e355ced732c694e5fde204
filip-develop <filip.develop@outlook.com>
2023-04-24 21:32:57 +0200

    parameter hardcoded edit

--- src/MessageHandler/EmailNotificationHandler.php
+++ src/MessageHandler/EmailNotificationHandler.php
@@ -3,23 +3,28 @@
 namespace App\MessageHandler;
 
 use App\Message\EmailNotification;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class EmailNotificationHandler implements MessageHandlerInterface
 {
-    private MailerInterface $mailer;
+    public function __construct(
+        private readonly ParameterBagInterface $parameterBag,
+        private readonly MailerInterface $mailer
+    ) { }
 
-    public function __construct(MailerInterface $mailer)
-    {
-        $this->mailer = $mailer;
-    }
-
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function __invoke(EmailNotification $message): void
     {
+        $defaultEmail = $this->parameterBag->get('default_email');
+
         $email = (new Email())
-            ->from('test@gmail.com')
+            ->from($defaultEmail)
             ->subject($message->getSubject())
             ->to($message->getRecipient())
             ->text($message->getContent());
